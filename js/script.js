34d96f412d9f5b49acfeb906cb9b1e167ccc199d
FraFra2 <kekkino41@gmail.com>
2023-06-18 23:32:03 +0200

    Email Fine

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 const myForm = document.getElementById("main-form");
-const myh1 = document.querySelector("h1");
-
+const myh2 = document.querySelector("h2");
+const emailInput = document.querySelector("emailInput");
+let valid, empty;
 
 const validEmails = ["prova@com", "pierpaolo@alice.it", "fierobecco@gmail.com", "soysauce@hotmail.com", "stanlio@olio.it"]
 
 myForm.addEventListener ("submit",
     function(event){
         event.preventDefault();
-        let userEmail = document.getElementById("emailInput");
+        console.log(emailInput);
+
+        let userEmail = document.getElementById("emailInput").value;
+        myh2.innerHTML = "";
         console.log(userEmail.value);
         let element;
         for (let index = 0; index < validEmails.length; index++) {
             element = validEmails[index];
+            if (element == userEmail){
+                valid = true;
+            }
+            else if(userEmail == ""){
+                empty = true;
+            }
         }
-        if (userEmail.value === element){
-            myh1.append("Email Valida");
+        if (valid){
+            myh2.append("Email Valida");
+            userEmail.value = "";
+
+        }
+        //!Questo con il required è inutile
+        else if(empty){
+            myh2.append("Prego inserire una mail");
             userEmail.value = "";
         }
         else{
-            myh1.append("Email Invalida");
+            myh2.append("Email Invalida");
             userEmail.value = "";
 
         }
-
-
     }
 
 
